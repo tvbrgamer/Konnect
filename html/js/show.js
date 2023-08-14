1b76c0b4f304cf353bb0449d1882107e42d33a4e
Jonathan Aleman <86426220+tvbrgamer@users.noreply.github.com>
2023-08-13 22:54:31 -0300

    sobre nós fim + coisas
    sobre nos finish + login ++

---
+++ html/js/show.js
@@ -0,0 +1,17 @@
+const senha = document.getElementById('senhaInput')
+const olha = document.getElementById('olho')
+
+var hidden = true
+
+olha.addEventListener("click", () => {
+    if (hidden == true) {
+        senha.type = "text"
+        hidden = false
+        olha.style.backgroundImage="url(./imagens/hidden.png)"
+    } else {
+        senha.type = "password"
+        hidden = true
+        olha.style.backgroundImage="url(./imagens/show.png)"
+    }
+}
+)
